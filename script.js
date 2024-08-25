ee6346c0aa254dc730f8f0469e13f8e5ee8cc86a
ganeshgummalla <ganeshgummalla255@gmail.com>
2024-08-25 21:54:49 +0530

    test

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault(); // Prevent the default form submission
+
+    // Simulate form submission success
+    alert('Thank you for your message. I will get back to you soon!');
+});
